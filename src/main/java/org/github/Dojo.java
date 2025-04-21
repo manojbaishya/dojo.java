f54dfb463f561dfddda6ae136e0bab315962dc49
Manoj Baishya <28330014+manojbaishya@users.noreply.github.com>
2025-04-21 19:47:36 +0530

    Modify main message

--- src/main/java/org/github/Dojo.java
+++ src/main/java/org/github/Dojo.java
@@ -2,10 +2,6 @@ package org.github;
 
 public class Dojo {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        System.out.println("Hello and welcome to Dojo.java, where we solve Computer Science problems in Java!");
     }
 }
